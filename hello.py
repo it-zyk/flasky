fac3c264cb17ee1fe3636f72dd2beac9f4497b2a
it-zyk <it-zyk@outlook.com>
2018-11-22 23:06:25 +0800

    Send Mail

--- hello.py
+++ hello.py
@@ -14,6 +14,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
 
+from flask_mail import Mail
+from flask_mail import Message
+
+from threading import Thread
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,6 +30,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = 'zhangykgwy@qq.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_ADMIN'] = 'zyk@cntcc.cn'
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[zyk]'
+app.config['FLASKY_MAIL_SENDER'] = 'zyk <zhangykgwy@qq.com'
+
+mail = Mail(app)
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -35,6 +53,19 @@ manager.add_command('db', MigrateCommand)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 class NameForm(FlaskForm):
@@ -74,6 +105,9 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -105,6 +139,5 @@ def make_shell_context():
 
 if __name__ == '__main__':
     # manager.add_command("shell", Shell(make_context=make_shell_context))
-    # app.run(debug=True)
+    app.run(debug=True)
     # manager.run()
-    manager.run()
